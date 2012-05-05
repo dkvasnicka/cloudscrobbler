dc017931bff76020c7c7f11efb433e79d06856bc
Daniel Kvasnicka <daniel.kvasnicka.jr@gmail.com>
2012-05-05 18:19:54 +0200

    refactoring

--- src/main/java/net/danielkvasnicka/cloudscrobbler/web/Settings.java
+++ src/main/java/net/danielkvasnicka/cloudscrobbler/web/Settings.java
@@ -4,13 +4,47 @@
  */
 package net.danielkvasnicka.cloudscrobbler.web;
 
-import javax.faces.bean.ManagedBean;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import net.danielkvasnicka.cloudscrobbler.listenermanagement.domain.Listener;
+import net.danielkvasnicka.cloudscrobbler.listenermanagement.repository.api.ListenerRepository;
+import net.danielkvasnicka.cloudscrobbler.web.auth.aop.LastFmSessionActive;
 
 /**
  *
  * @author daniel
  */
-@ManagedBean
-public class Index {
+@Named
+@RequestScoped
+@LastFmSessionActive
+public class Settings {
+
+    @Inject
+    private ListenerRepository listenerRepository;
+    
+    private Listener listener;
+    
+    @PostConstruct
+    public void initListener() {
+        this.listener = new Listener();
+    }
+    
+    public void save() {
+        
+        this.listenerRepository.saveListener(this.listener);
+    }
+    
+    // ----- get / set ------
+    
+    public Listener getListener() {
+        return listener;
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+    
     
 }
