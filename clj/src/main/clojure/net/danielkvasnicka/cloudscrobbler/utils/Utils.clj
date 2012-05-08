1f446d86c2a416601e33ee613af466d264214ade
Daniel Kvasnicka <daniel.kvasnicka.jr@gmail.com>
2012-05-08 22:07:41 +0200

    refactored some of the logic to clojure

--- clj/src/main/clojure/net/danielkvasnicka/cloudscrobbler/utils/Utils.clj
+++ clj/src/main/clojure/net/danielkvasnicka/cloudscrobbler/utils/Utils.clj
@@ -1,6 +1,14 @@
 (ns net.danielkvasnicka.cloudscrobbler.utils.Utils
 	(:gen-class
-      :methods [#^{:static true} [getOnlyTracksFromMixSections [java.util.Collection] java.util.Collection]]))
+      :methods 
+          [
+           #^{:static true} [getOnlyTracksFromMixSections [java.util.Collection] java.util.Collection]
+           #^{:static true} [getListenTimeForNewestMix [java.util.Collection] java.util.Date] 
+          ]
+  ))
 
 (defn -getOnlyTracksFromMixSections [sections]
   (map #(.getTrack %) (filter #(.isTrack %) sections)))
+
+(defn -getListenTimeForNewestMix [mixes]
+  (last (sort #(.getListenTime %) mixes)))
