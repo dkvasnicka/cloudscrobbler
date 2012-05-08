c923c49fa0a656b3717350d6e31a150438177c33
Daniel Kvasnicka <daniel.kvasnicka.jr@gmail.com>
2012-05-08 13:07:49 +0200

    Working Mixcloud track loading & Maven structure overhaul - added clojure module

---
+++ app/src/main/java/net/danielkvasnicka/cloudscrobbler/clouds/mixcloud/domain/MixTrack.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.danielkvasnicka.cloudscrobbler.clouds.mixcloud.domain;
+
+import java.util.Map;
+import net.danielkvasnicka.cloudscrobbler.engine.api.Track;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+/**
+ *
+ * @author daniel
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MixTrack implements Track {
+
+    private String name;
+    
+    @JsonProperty("artist")
+    private Map artistInfo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map getArtistInfo() {
+        return artistInfo;
+    }
+
+    public void setArtistInfo(Map artistInfo) {
+        this.artistInfo = artistInfo;
+    }
+
+    public String getArtist() {
+        return (String) this.artistInfo.get("name");
+    }        
+}
---
+++ app/src/main/java/net/danielkvasnicka/cloudscrobbler/listenermanagement/repository/MongoDBListenerRepository.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.danielkvasnicka.cloudscrobbler.listenermanagement.repository;
+
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import net.danielkvasnicka.cloudscrobbler.listenermanagement.config.Collection;
+import net.danielkvasnicka.cloudscrobbler.listenermanagement.domain.Listener;
+import net.danielkvasnicka.cloudscrobbler.listenermanagement.repository.api.ListenerRepository;
+import net.danielkvasnicka.cloudscrobbler.web.auth.aop.LastFmSessionActive;
+import org.jongo.MongoCollection;
+
+/**
+ *
+ * @author daniel
+ */
+@Named @Singleton
+public class MongoDBListenerRepository implements ListenerRepository {
+
+    @Inject @Collection("listeners")
+    private MongoCollection listeners;
+    
+    @LastFmSessionActive
+    public Listener findListener(String lastFmSessionKey) {
+        return this.listeners.findOne(
+                "{ lastFmSessionKey : # }", lastFmSessionKey).as(Listener.class);
+    }
+
+    public void saveListener(Listener listener) {
+        try {
+            this.listeners.save(listener);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public Iterable<Listener> getAllActiveListeners() {
+        return this.listeners.find("{ enableScrobbling : true }").as(Listener.class);
+    }
+}
